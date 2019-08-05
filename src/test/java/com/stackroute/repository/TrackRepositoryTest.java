a3521b7631cc719b53a0976304c6226b06c896c4
adithyaGowda <adithyagowda5696@gmail.com>
2019-08-05 18:28:55 +0530

    updating current status

--- src/test/java/com/stackroute/repository/TrackRepositoryTest.java
+++ src/test/java/com/stackroute/repository/TrackRepositoryTest.java
@@ -1,17 +1,22 @@
 package com.stackroute.repository;
 
 import com.stackroute.domain.Track;
+import com.stackroute.exception.TrackAlreadyExistsException;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
 
 import static org.junit.Assert.*;
 
 @RunWith(SpringRunner.class)
-@DataMongoTest
+@SpringBootTest
 public class TrackRepositoryTest {
 
     @Autowired
@@ -20,9 +25,37 @@ public class TrackRepositoryTest {
 
     @Before
     public void setUp() throws Exception {
+        track = new Track();
+        track.setId(1);
+        track.setName("Riverside");
+        track.setComments("Time travelling");
+        trackRepository.save(track);
     }
 
     @After
     public void tearDown() throws Exception {
+        trackRepository.deleteAll();
+    }
+
+    @Test
+    public void givenInputShouldSaveTheTrack(){
+
+        Track testTrack = trackRepository.findById(track.getId()).get();
+        assertEquals(track,testTrack);
+    }
+
+    @Test
+    public void givenInputShouldNotSaveAnyTracks(){
+
+       Track newTrack = new Track(1,"Beautiful","Restful Evening");
+       trackRepository.save(newTrack);
+    }
+
+    @Test
+    public void givenInputObjectShouldDeleteById(){
+
+        List<Track> testTrack = trackRepository.findAll();
+
+//        assertEquals(testTrack,trackRepository.);
     }
 }
